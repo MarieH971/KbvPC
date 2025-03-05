5e1a26d1daa00ff6ab23d71a1ce28014ccbb77f6
MarieH971 <mhbasse@gmail.com>
2025-03-05 11:20:30 -0400

    Insertion page react inscription ok

---
+++ assets/react/controllers/Inscription.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Inscription = () => {
+    return (
+        <div>
+            <h1>Page d'Inscription</h1>
+            <p>Formulaire d'inscription</p>
+        </div>
+    );
+};
+
+export default Inscription;
