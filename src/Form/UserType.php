b47ed185f6b884a16d8515fdc242dca45e5313bd
MarieH971 <mhbasse@gmail.com>
2025-03-12 23:44:54 -0400

    mise en page LoginPage - Tentative MEP newPage - Ajout photo Home -About

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,7 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
-use App\Enum\UserRole;
+
 use App\Enum\Level;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
