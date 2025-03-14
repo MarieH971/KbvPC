e579e46da839a0716bc6dfaafca37a88b017d523
Galoyer Marc <mgaloyer@uneak.fr>
2025-03-14 11:48:18 -0400

    fix form

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class InscriptionController extends AbstractController
@@ -13,6 +18,53 @@ final class InscriptionController extends AbstractController
     {
         return $this->render('inscription/inscription.html.twig', [
             'controller_name' => 'InscriptionController',
+        ]);
+    }
+
+
+    #[Route('/inscription/formulaire', name: 'app_inscription_form', methods: ['GET', 'POST'])]
+    public function inscriptionForm(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response
+    {
+        $user = new User();
+        $action = $this->generateUrl('app_user_new');
+        return $this->handleForm($request, $entityManager, $passwordHasher, $action, $user, 'app_user_list');
+    }
+
+    protected function handleForm(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher,
+        string $action,
+        User $user,
+        ?string $redirect = null,
+
+    ): Response {
+        $form = $this->createForm(UserType::class, $user, ['action' => $action, 'method' => 'POST']);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $plainPassword = $user->getPassword();
+            $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+            $user->setPassword($hashedPassword);
+
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+
+            return $redirect
+                ? $this->redirectToRoute($redirect, [], Response::HTTP_SEE_OTHER)
+                : $this->redirectToRoute('app_user_list', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('inscription/formulaire.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user,
         ]);
     }
 }
